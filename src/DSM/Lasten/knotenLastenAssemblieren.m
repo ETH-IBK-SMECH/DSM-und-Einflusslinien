fe93cb522953b7200857bfd29dd398658f9546cc
oakbergere <emaximili@student.ethz.ch>
2025-10-21 11:21:20 +0200

    vectorized knotenLastenAssemblieren: -replaced for-loop -DOF-mapping using logical mask

--- src/DSM/Lasten/knotenLastenAssemblieren.m
+++ src/DSM/Lasten/knotenLastenAssemblieren.m
@@ -1,20 +1,23 @@
 function F_N = knotenLastenAssemblieren(model, DOF, nDOF)
 % System-Knotenlastvektor
-F_N = sparse(nDOF,1);
 
 if ~isfield(model,'KnotenLast') || isempty(model.KnotenLast)
+    F_N = sparse(nDOF,1);
     return;
 end
+
+DOF = DOF(:);
 nkd = model.Info.nKnotenDOF;
 
-for i = 1:numel(model.KnotenLast)
-    L = model.KnotenLast(i);
-    if ~isfield(L,'node') || ~isfield(L,'dir') || ~isfield(L,'val'), continue; end
-    if ~isscalar(L.node) || ~isscalar(L.dir), continue; end
-    localIdx = (L.node-1)*nkd + L.dir;
-    g = safeDOF(localIdx, DOF);
-    if g ~= 0
-        F_N(g) = F_N(g) + L.val;
-    end
-end
+KL = model.KnotenLast(:);
+node = [KL.node]'; 
+dir  = [KL.dir]'; 
+val = [KL.val]';
+
+local = (node-1)*nkd + dir;                 % vector
+ok = local >= 1 & local <= numel(DOF);
+g = zeros(size(local)); 
+g(ok) = DOF(local(ok));
+keep = g > 0;
+F_N = sparse(g(keep), 1, val(keep), nDOF, 1); 
 end
